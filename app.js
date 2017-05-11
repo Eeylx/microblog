1073f10df2c7718f1d2fbf3f422c9db9bfb9c7f8
liyuxuan <liyuxuan>
2017-05-11 22:58:42 +0800

    添加处理表单及文件上传的中间件

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');     // 用于设置网站图标
+var favicon = require('serve-favicon');     // 设置网站图标
 var logger = require('morgan');             // 关于http请求的日志中间件
 var cookieParser = require('cookie-parser');    // 处理请求中的 cookie
-var bodyParser = require('body-parser');    // 用于解析 req.body 中的内容
+var bodyParser = require('body-parser');    // 解析 req.body 中的内容
 var session = require('express-session');   // session
 var mongoStore = require('connect-mongo')(session); // 用于存储 session
-var flash = require('connect-flash');       // 用来在网页上显示通知
+var flash = require('connect-flash');       // 在网页上显示通知
+var formidable = require('express-formidable'); // 处理表单及文件上传的中间件
 
 var config = require('config-lite')(__dirname); // 查找该目录下的config文件夹, 再查找default文件
 var routes = require('./routes/index');     // 路由
@@ -24,7 +25,7 @@ app.set('view engine', 'ejs');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 // 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
@@ -46,6 +47,12 @@ app.use(session({
 // flash 中间件, 用来显示通知, 基于 session, 故需要放在 session 之后
 app.use(flash());
 
+// 处理表单及文件上传的中间件
+app.use(formidable({
+    uploadDir: path.join(__dirname, 'public', 'images'),    // 上传文件目录
+    keepExtensions: true    // 保留后缀
+}));
+
 // 设置模板全局常量, app.locals 上通常挂载常量信息
 app.locals.blog = {
     title: pkg.name,
@@ -62,21 +69,21 @@ app.use(function (req, res, next) {
 app.use(routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
