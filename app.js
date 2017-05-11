b318a199dc7b780d3e6afc73819b94461b0ce52b
Eeylx <137774892@qq.com>
2017-05-11 17:33:29 +0800

    设置模板的全局常量, 避免每次调用res.render时传参

--- app.js
+++ app.js
@@ -19,6 +19,18 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 // 设置模板引擎为 ejs
 app.set('view engine', 'ejs');
+// 设置模板全局常量, app.locals 上通常挂载常量信息
+app.locals.blog = {
+    title: pkg.name,
+    description: pkg.description
+};
+// 添加模板必须的常量, res.locals 上通常挂载变量信息
+app.use(function (req, res, next) {
+    res.locals.user = req.session.user;
+    res.locals.success = req.flash('success').toString();
+    res.local.error = req.flash('error').toString();
+    next();
+});
 
 // 设置网站图标
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
