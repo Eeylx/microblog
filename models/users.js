381331946854cdc43e4a7860c377f564edbb0b67
liyuxuan <liyuxuan>
2017-05-12 11:33:18 +0800

    添加了注释

--- models/users.js
+++ models/users.js
@@ -1,7 +1,14 @@
+/**
+ * @Author: lyx
+ * @Date: 2017/5/11
+ */
+'use strict';
+
 var User = require('../lib/mongo').User;
 
 module.exports = {
 
+    // 注册 创建一个新用户
     create: function create(user){
         return User.create(user).exec();
     }
