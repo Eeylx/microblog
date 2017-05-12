cdf7e9c3b7b0eb2b3e5639d13c51d81fc76dfc09
liyuxuan <liyuxuan>
2017-05-12 11:32:56 +0800

    将router中的操作移动到controller文件夹下的对应文件中, router只负责匹配和调用函数

--- routes/reg.js
+++ routes/reg.js
@@ -8,29 +8,16 @@ var express = require('express');
 var router = express.Router();
 
 var check = require('../middlewares/check');
-
+var reg = require('../controller/reg');
 
 // 检测是否未登录, 在未登录状态下才能注册
 router.use(check.checkNotLogin);
 
 // GET /reg 注册页
-router.get('/', function (req, res) {
-    res.render('reg');
-});
+router.get('/', reg.regg);
 
 // POST /reg 用户注册
-router.post('/', function (req, res) {
-    var name = req.fields.name;
-    var password = req.fields.password;
-    var repassword = req.fields.repassword;
-    var gender = req.fields.gender;
-    var bio = req.fields.bio;
-    var avatar = req.files.avatar.path.split(path.sep).pop();
-
-    
-
-    res.render(req.flash());
-});
+router.post('/', reg.register);
 
 
 module.exports = router;
